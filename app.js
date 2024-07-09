3bde8d693834b8d32b40f9f585dad5cb48520cab
Dnyanesh Pise <gitdnyanesh@hotmail.com>
2024-07-09 23:45:22 +0530

    basic routes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path =require('path');
 const mongoose = require('mongoose');
-const Campground = require('./models/campground')
+const Campground = require('./models/campground');
+const exp = require('constants');
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     .then(() => {
         console.log("Connected to MongoDB");
@@ -16,17 +17,31 @@ const app = express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
 
+app.use(express.urlencoded({ extended:true }))
 
 app.get('/',(req,res)=>{
   res.render('home')
 })
 
-app.get('/makecampground',async (req,res)=>{
-  const camp = new Campground({
-    title:'My Backyard',description: 'cheap camping'
-  })
-  await camp.save();
-  res.send(camp)
+app.get('/campgrounds', async (req,res)=>{
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index' , { campgrounds })
+})
+
+app.get('/campgrounds/new',(req , res)=>{
+  res.render('campgrounds/new')
+})
+
+app.get('/campgrounds/:id', async (req,res)=>{
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render('campgrounds/show' , { campground })
+})
+
+app.post('/campgrounds',async (req ,res)=>{
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`)
 })
 
 app.listen(8080,()=>{
